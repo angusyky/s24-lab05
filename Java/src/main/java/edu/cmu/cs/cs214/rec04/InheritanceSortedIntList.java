c9d0442d42dd199db042803ffb38fd1d68c36872
angusyky <angusyiuky@gmail.com>
2024-02-13 15:52:01 -0500

    Task 1: Inheritance sorted int list

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,16 +1,30 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
+ * InheritanceSortedIntList -- a variant of a SortedIntList that keeps count of the number of
+ * attempted element insertions (not to be confused with the current size, which goes down when an
+ * element is removed) and exports an accessor (totalAdded) for this count.
  *
  * @author Nora Shoemaker
- *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
 
+  // Write your implementation below with API documentation
+  private int totalAdded;
+
+  public InheritanceSortedIntList() {
+    super();
+    totalAdded = 0;
+  }
+
+  @Override
+  public boolean add(int num) {
+    totalAdded++;
+    return super.add(num);
+  }
+
+  public int getTotalAdded() {
+    return totalAdded;
+  }
 }
